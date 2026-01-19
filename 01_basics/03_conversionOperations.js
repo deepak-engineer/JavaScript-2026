3f5c4ccf36d303762a44401a9f9210e3a87c65a8
deepak-engineer <devwebwithdeepak@gmail.com>
2026-01-19 14:26:15 +0530

    add conversion and operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "11"
+
+// know the type why because you dont know the type if comes from backend
+
+console.log(typeof score)
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+// "33" -> 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 = true/ 0 = false
+// '' = false , 'ada' = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+
