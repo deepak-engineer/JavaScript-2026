ef9df029f4bffca517ca4d782bae4515d071c65c
deepak-engineer <devwebwithdeepak@gmail.com>
2026-01-19 17:12:14 +0530

    add dataType summary

---
+++ 01_basics/04_dataType-summary.js
@@ -0,0 +1,29 @@
+//Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol
+
+const str1 = "Deepak" // string
+const age = 25 // Number
+const isLoggedIn = false // Boolean
+const outsideTemp = null
+let userEmail
+
+const id = Symbol("123")
+const userId = Symbol("123") // if its looks same does'nt mean value is same
+
+const bigNumber = 2111112312323123412n
+
+// Refrence (Non-Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"] // Array
+
+let myObj = {
+  name: "Deepak",
+  age: 25,
+}
+
+const myFunction = function () {
+  console.log("Hello World"  )
+}
