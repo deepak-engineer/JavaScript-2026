75d3943ef4de53dc0a063c509407017792e0f05e
deepak-engineer <devwebwithdeepak@gmail.com>
2026-01-19 13:58:44 +0530

    add variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1144443
+let accountEmail = "devwebwithdeepak@gmail.com"
+var accountPassword = "12345"
+
+accountCity = "Saharanpur" // in js declared without keyword
+
+let accountState // in js if you declared the variable and dont initilize the value js will know as undefined
+console.table([
+  accountEmail,
+  accountPassword,
+  accountId,
+  accountCity,
+  accountState,
+])
