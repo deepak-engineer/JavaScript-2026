259309c8bfad0fde9d800087dcecc5796147e775
deepak-engineer <devwebwithdeepak@gmail.com>
2026-01-19 14:14:15 +0530

    add datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+// "use strict" // treat all js code ad newer version
+
+// but nowdays you don't need to write use strict
+// js will understand automatically
+
+// alert("hello") we are using nodejs , not browser
+
+   
+
+let name = "Deepak"
+let age = 25
+let isLoggedIn = true
+
+// data types -> primitive data types
+
+//number => 2 to power 53
+// bigint //not used lessly use
+// string => "", '' use best practice
+// boolean => true/false
+// null => standalone value mtlb value khali hai '
+// undefined
+// symbol => unique
+
+
+//object 
+
+// console.log(typeof null) object type of for see type
+
