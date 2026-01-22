f578a29955823d1a04b6a934f3d461f77605d8a9
deepak-engineer <devwebwithdeepak@gmail.com>
2026-01-22 20:19:28 +0530

    add clouser

--- 05_scopes/01_scopes.js
+++ 05_scopes/01_scopes.js
@@ -18,3 +18,23 @@ var c = 30
 console.log(c); // but it is available out of the block so please dont use var
 
 
+// nested scopes
+
+function one(){
+  const username= 'deepak'
+  function two(){
+    const website = 'youtube'
+    console.log(username);
+  }
+
+  console.log(website);
+  two()
+  
+}
+one()
+
+//+++++++++++++++++ interesting ++++++++++++++
+
+function addone(num){
+  return num + 1
+}
